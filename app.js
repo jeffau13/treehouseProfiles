b163578e15fae91402ca2e5ee4389b591cd182cf
jeffau13 <jeffau13@gmail.com>
2019-02-20 21:32:25 -0500

    created simple web server that responds with current time per second

--- app.js
+++ app.js
@@ -17,3 +17,18 @@
         merge values into string
 
 */
+
+//1)
+
+const http = require('http');
+http
+  .createServer(function(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    setInterval(function() {
+      response.write(new Date() + '\n');
+    }, 1000);
+
+    // response.end('Hello World \n');
+  })
+  .listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.1:1337');
