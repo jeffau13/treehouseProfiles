3f3218f91fc08629efd5b23244100c7c47eecfa5
jeffau13 <jeffau13@gmail.com>
2019-02-20 20:21:04 -0500

    init commit, created TODO in app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+/* TODO:
+1) Create web server
+2) Handle HTTP GET / and POST eg Home
+    if url === '/' && GET
+        show search
+    if url == '/' && POST
+        redirect to /:username    
+3) Handle HTTP route Get /: username eg: /jeffau
+    if url == "/..."
+        get json from treehouse
+            on 'end'
+                show profile
+            on 'error'
+                show error
+4) Function that handles the reading of files and merge in value
+    read from file, get a string
+        merge values into string
+
+*/
